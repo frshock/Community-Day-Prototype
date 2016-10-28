8554e73f9f6ba5e949dfde49567bf788860f5a02
frshock <will.s.crane@gmail.com>
2016-10-28 16:02:45 -0400

    Community Day final

--- app.js
+++ app.js
@@ -2,19 +2,53 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const cheerio = require('cheerio');
-const mapFunction = require('public/mapFunction.js');
-const PORT = 8060;
+const PORT = 8040;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
-    console.log(mapFunction);
-
-    var options = { method: 'GET',
+    let resultList = [];
+    let options = { method: 'GET',
     url: 'https://api.yelp.com/v3/businesses/search?term=mexican',
-    qs: { text: 'del', latitude: 43.653226, longitude: -79.38318429999998},
+    qs: { 
+        text: 'del',
+        latitude: 43.653226, 
+        longitude: -79.38318429999998
+    },
+    headers: 
+    { 'postman-token': 'a6edbc93-ad5c-6278-6bc4-8df9fb23e4b3',
+        'cache-control': 'no-cache',
+        authorization: 'Bearer Drp4lYzfTpNBy9iz3kzMZ93nzlS1n9srsRuT5NZrGR0FYNmOO70zNv4NoF7vE_USZITU6trAhzguLvIZICMNJXFAOtSGdMW_AIVyv5mbgLhl08ddR2OsByVk_HkKWHYx',
+        'content-type': 'application/x-www-form-urlencoded' },
+    form: 
+    { client_id: 'mAyGa5abEH5SwVYpi528aA',
+        client_secret: 'h3YGbygR9UPp2EGDzHB9aJPpyA1Wm9dRqrKNQYxdhgO9MHMCaZfubt5T9PBApr4J',
+        access_token: 'Drp4lYzfTpNBy9iz3kzMZ93nzlS1n9srsRuT5NZrGR0FYNmOO70zNv4NoF7vE_USZITU6trAhzguLvIZICMNJXFAOtSGdMW_AIVyv5mbgLhl08ddR2OsByVk_HkKWHYx' }
+    }; 
+
+    request(options, function(err, response, body){
+        if(!err){
+            let $ = cheerio.load(body);
+            let parsedObj = JSON.parse(body);
+                res.render('index', {resultList: parsedObj.businesses});     
+        }
+        else{
+            console.log(err);
+        }
+    })
+})
+
+app.get('/search/:lat&:lng', (req, res) => {
+    var lati = req.params.lat;
+    var long = req.params.lng;
+    let options = { method: 'GET',
+    url: 'https://api.yelp.com/v3/businesses/search?term=mexican',
+    qs: {
+        text: 'del',
+        latitude: lati,
+        longitude: long,
+        },
     headers: 
     { 'postman-token': 'a6edbc93-ad5c-6278-6bc4-8df9fb23e4b3',
         'cache-control': 'no-cache',
@@ -26,19 +60,16 @@ app.get('/', (req, res) => {
         access_token: 'Drp4lYzfTpNBy9iz3kzMZ93nzlS1n9srsRuT5NZrGR0FYNmOO70zNv4NoF7vE_USZITU6trAhzguLvIZICMNJXFAOtSGdMW_AIVyv5mbgLhl08ddR2OsByVk_HkKWHYx' }
     };
 
-
     request(options, function(err, response, body){
         if(!err){
             let $ = cheerio.load(body);
             let parsedObj = JSON.parse(body);
-            console.log(require('/mapFunction.js'));
-            console.log(parsedObj);
+            res.render('search', {resultList: parsedObj.businesses, axisY: lati, axisX: long});
         }
         else{
             console.log(err);
         }
     })
-    res.render('index');
 })
 
 app.listen(PORT, () => {
--- public/mapFunction.js
+++ public/mapFunction.js
@@ -1,11 +1,6 @@
-// module.exports = [43.653226, -79.38318429999998];
- 
-
-tempArray = [43.653226, -79.38318429999998]; 
-
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
+        zoom: 12,
         center: {lat: 43.653226, lng: -79.38318429999998}
     });
     var geocoder = new google.maps.Geocoder();
@@ -13,18 +8,18 @@ function initMap() {
     document.getElementById('submit').addEventListener('click', function() {
         geocodeAddress(geocoder, map);
     });
-    }
+}
 
-    function geocodeAddress(geocoder, resultsMap) {
+function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('address').value;
     geocoder.geocode({'address': address}, function(results, status) {
         if (status === 'OK') {
         resultsMap.setCenter(results[0].geometry.location);
 
-        tempArray[0] = results[0].geometry.location.lat();
-        tempArray[1] = results[0].geometry.location.lng();
+        let currentLat = results[0].geometry.location.lat();
+        let currentLong = results[0].geometry.location.lng();
 
-        console.log(tempArray);
+        window.location.href = '/search/'+currentLat+'&'+currentLong;
 
         var marker = new google.maps.Marker({
             map: resultsMap,
@@ -35,5 +30,3 @@ function initMap() {
         }
     });
 }
-
-module.exports = tempArray;
